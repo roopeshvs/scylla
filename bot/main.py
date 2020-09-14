88f17411770bc1363bd5a60e364e874fc6fbaaf4
Prasad Narkhede <5206906-p014ri5@users.noreply.gitlab.com>
2020-09-14 12:16:59 +0530

    Done somewhat basic stuff

--- bot/main.py
+++ bot/main.py
@@ -8,22 +8,44 @@ import os
 
 import datetime
 
-# ANCHOR INITIALIZING VARIABLES
+# ANCHOR TOKEN, DESCRIPTION, ACTIVITY AND PREFIX
 
-token = os.getenv('SCYLLA_TOKEN')
-description = '''
+token = os.getenv("SCYLLA_TOKEN")
+description = """
 A community driven Discord Bot made for programmers, developers and hackers.
-'''
+"""
+prefix = ">"
+custom_status = prefix + "help"
 
 # ANCHOR MAIN FUNCTION
 
-if __name__ == '__main__':
 
-	bot = Bot(command_prefix='>', description=description)
+bot = commands.Bot(command_prefix=prefix, description=description)
+
+@bot.event
+async def on_ready():
+	print("--------------------")
+	print("Bot is online!")
+	print("Build successful 🚀")
+	print(f"Logged in as {bot.user.name}")
+	print("--------------------")
+	print("Setting status and activity")
+	await bot.change_presence(status=discord.Status.online, type=1, activity=discord.Game(custom_status))
+	print("Status set to")
+
+@bot.command()
+async def load(ctx, extension):
+	bot.load_extension(f'cogs/{extension}.extension')
+
+@bot.command()
+async def unload(ctx, extension):
+	bot.unload_extension(f'cogs.{extension}.{extension}')
 
 
-	try:
-		 bot.run(token)
-	except KeyboradInterrupt:
-		 bot.logout()
+
+
+try:
+	 bot.run(token)
+except KeyboradInterrupt:
+	 bot.logout()
 
